4e6754cacde8b635252ffedd7d5db295cd7e0765
EzequielCamino <ezeeqii@gmail.com>
2022-12-12 19:56:46 -0300

    Finalizado. Valida los campos.

--- main.js
+++ main.js
@@ -6,18 +6,20 @@ class ProductManager {
     addProduct(title, description, price, thumbnail, code, stock) {
         if(this.products.find((product) => product.code === code)) {
             return console.log("Code assigned");
-        } else {
+        } else if(title && description && price && thumbnail && stock) {
             ProductManager.idCreator++;
             const id = ProductManager.idCreator
             const product = {id, title, description, price, thumbnail, code, stock}
             this.products.push(product);
+        } else {
+            return console.log("Faltan completar datos")
         }
     }
     getProducts() {
-        return console.log(this.products)
+        return this.products;
     }
     getProductById(id){
-        return console.log(this.products.find((product) => product.id === id) || "Not found");
+        return this.products.find((product) => product.id === id) || "Not found"
     }
 }
 
@@ -25,10 +27,10 @@ class ProductManager {
 /* TESTING CODE */
 
 /* let newProducts = new ProductManager;
-newProducts.addProduct("a", "b", 10, "no", 45, 20);
-newProducts.addProduct("a", "b", 10, "no", 45, 20);
-newProducts.addProduct("a", "b", 10, "no", 54, 20);
 newProducts.getProducts()
-newProducts.getProductById(2);
+newProducts.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+newProducts.getProducts()
+newProducts.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+newProducts.addProduct("producto prueba2", "Este es otro producto prueba", 200, "Sin imagen", "abc1234", 25);
 newProducts.getProductById(5);
- */
+newProducts.getProductById(2); */
